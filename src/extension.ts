ccc95fd39acfa5f717d951d64e6b4d4dfffcd2b6
Harsh_G <hgaur491@gmail.com>
2026-01-24 23:28:34 +0530

    feat: remove unused activation events and code

--- src/extension.ts
+++ src/extension.ts
@@ -1,64 +1,55 @@
 import * as vscode from "vscode";
 import { API, GitExtension, Repository } from "./types/git";
-const generateCommitMessage = (): string => {
-    return "<feat: ai commit message>";
-};
-const scmActionGenerateCommitMessage = async (repo: Repository) => {
-    const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-    status.text = "$(sync~spin) Generating commit message...";
-    status.show();
-    try {
-        const diffs = await repo.diffIndexWithHEAD();
 
-        if (!diffs) {
-            vscode.window.showInformationMessage("No staged changes to analyze.");
-            return;
-        }
+const generateCommitMessage = async (repo: Repository) => {
+    const diffs = await repo.diffIndexWithHEAD();
 
-        // TODO: get context
-        for (const diff of diffs) {
-            console.log("diff:", diff);
-        }
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-        // TODO: set ai response in the commit input box
-        repo.inputBox.value = generateCommitMessage();
-    } finally {
-        status.dispose();
-    }
-};
-
-export const activate = async (context: vscode.ExtensionContext) => {
-    const gitExtension = vscode.extensions.getExtension<GitExtension>("vscode.git");
-
-    if (!gitExtension) {
-        vscode.window.showErrorMessage("onyxExtension: Git extension not enabled");
+    if (!diffs) {
+        vscode.window.showInformationMessage("No staged changes to analyze.");
         return;
     }
-    const gitExports = await gitExtension.activate();
-    const gitApi: API = gitExports.getAPI(1);
-    const repo = gitApi.repositories[0];
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand("onyxExtension.scmAction", async () => {
+    // TODO: get context
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    for (const diff of diffs) {
+        console.log("diff:", diff);
+    }
+    // TODO: set ai response in the commit input box
+    repo.inputBox.value = "<feat: ai commit message>";
+};
+
+const scmActionGenerateCommitMessage = async () => {
+    await vscode.window.withProgress(
+        {
+            location: vscode.ProgressLocation.Notification,
+            title: "Generating commit message with AI...",
+            cancellable: false,
+        },
+        async () => {
             await vscode.window.withProgress(
                 {
-                    location: vscode.ProgressLocation.Notification,
+                    location: vscode.ProgressLocation.SourceControl,
                     title: "Generating commit message with AI...",
                     cancellable: false,
                 },
                 async () => {
-                    await vscode.window.withProgress(
-                        {
-                            location: vscode.ProgressLocation.SourceControl,
-                            title: "Generating commit message with AI...",
-                            cancellable: false,
-                        },
-                        async () => {
-                            await scmActionGenerateCommitMessage(repo);
-                        },
-                    );
+                    const gitExtension = vscode.extensions.getExtension<GitExtension>("vscode.git");
+
+                    if (!gitExtension) {
+                        vscode.window.showErrorMessage("onyxExtension: Git extension not enabled");
+                        return;
+                    }
+                    const gitExports = await gitExtension.activate();
+                    const gitApi: API = gitExports.getAPI(1);
+                    const repo = gitApi.repositories[0];
+                    await generateCommitMessage(repo);
                 },
             );
-        }),
+        },
+    );
+};
+export const activate = async (context: vscode.ExtensionContext) => {
+    context.subscriptions.push(
+        vscode.commands.registerCommand("onyxExtension.scmAction", scmActionGenerateCommitMessage),
     );
 };
