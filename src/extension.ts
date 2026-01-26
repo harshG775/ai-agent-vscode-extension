c32720f260e9cba476bf29553afbac7b1c941f2b
Harsh_G <hgaur491@gmail.com>
2026-01-26 14:24:09 +0530

    feat: analyze staged changes per file

--- src/extension.ts
+++ src/extension.ts
@@ -3,16 +3,46 @@ import { GitExtension, Repository } from "./types/git";
 
 const generateCommitMessage = async (repo: Repository) => {
     try {
-        const diff = await repo.diff(true);
-
-        if (!diff || diff.trim() === "") {
-            vscode.window.showInformationMessage("No staged changes to analyze.");
+        const changes = await repo.diffIndexWithHEAD();
+        if (changes.length <= 0) {
+            vscode.window.showInformationMessage("No changes changes to analyze.");
             return;
         }
+        //
+        let finnalDiff = "";
+        for (const diff of changes) {
+            const fileDiff = await repo.diffIndexWithHEAD(diff.uri.fsPath);
+            if (!fileDiff || fileDiff.trim() === "") {
+                vscode.window.showInformationMessage("No staged file to analyze.");
+                continue;
+            }
 
-        console.log("Staged Diff for AI:\n", diff);
+            finnalDiff += fileDiff;
+        }
+        const messages = [
+            {
+                role: "system",
+                content:
+                    "You are a helpful assistant that generates informative git commit messages based on git diffs output. Skip preamble and remove all backticks surrounding the commit message.",
+            },
+            {
+                role: "user",
+                content: `
+                Based on the provided git diff, generate a concise and descriptive commit message.
 
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+                The commit message should:
+                1. Has a short title (50-72 characters)
+                2. The commit message should adhere to the conventional commit format
+                3. Describe what was changed and why
+                4. Be clear and informative
+
+                'git --no-pager diff --staged --diff-filter=d' Output:
+                ${finnalDiff}
+                `,
+            },
+        ];
+        console.log("Staged Diff for AI$$$:\n", messages);
+
         repo.inputBox.value = "feat: logic implemented via ai";
     } catch (err) {
         vscode.window.showErrorMessage("Failed to get diff: " + err);
